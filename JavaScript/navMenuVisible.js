09d6955fad3227936baaf54041df92e5b36ec65a
Salwa <44788541+SalwaToo@users.noreply.github.com>
2022-06-23 12:20:57 -0400

    Update navMenuVisible.js

--- JavaScript/navMenuVisible.js
+++ JavaScript/navMenuVisible.js
@@ -1,5 +1,5 @@
-const navMenu = document.querySelector("nav-menu");
-const navToggle = document.querySelector("nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+const navToggle = document.querySelector(".nav-toggle");
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible")
