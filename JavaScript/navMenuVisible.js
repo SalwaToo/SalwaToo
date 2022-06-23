ec3fce6e1a32335670dd43a7fa86540bfb5da892
Salwa <44788541+SalwaToo@users.noreply.github.com>
2022-06-23 12:04:55 -0400

    Update navMenuVisible.js

--- JavaScript/navMenuVisible.js
+++ JavaScript/navMenuVisible.js
@@ -2,9 +2,9 @@ const navMenu = document.querySelector(".nav-menu");
 const navToggle = document.querySelector(".nav-toggle");
 
 navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle(".nav-menu_visible")
+    navMenu.classList.toggle("nav-menu_visible")
 
-    if(navMenu.classList.contains(".nav-menu_visible")) {
+    if(navMenu.classList.contains("nav-menu_visible")) {
         navToggle.setAttribute("aria-label", "Abrir Menú")
     } else {
         navToggle.setAttribute("aria-label", "Cerrar Menú")
